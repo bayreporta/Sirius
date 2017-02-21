9af8fdac9a72e5e4aab7e694d5a466d251837dd3
John Osborn <bayreporta@gmail.com>
2017-02-21 14:00:52 -0800

    initial footer

--- footer.php
+++ footer.php
@@ -2,8 +2,9 @@
 </div>
 <footer id="footer" role="contentinfo">
 	<div id="copyright">
-		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'laniakea' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+		<?php echo sprintf( __( '%1$s %2$s %3$s', 'sirius' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
 	</div>
+	<div id="contact"></div>
 </footer>
 </div>
 <?php wp_footer(); ?>
